61f060f71b255def85e98d8a8ea1e6af8c7a7e2b
oauit <aidanjade.ward@gmail.com>
2022-09-28 11:50:14 -0600

    submission for deadline

--- src/index.js
+++ src/index.js
@@ -1,1 +1,72 @@
 // write your code here
+// write your code here
+// I am feeling very lost, without a learn test I didn't really know where to start or go. So I kept starting and aborting. Would love a review on how to start something like this.
+ 
+ 
+ 
+ 
+const url = 'http://localhost:3000/ramens'
+ 
+fetch(url)
+ 
+ 
+const ramenMenuContainer = document.getElementById('ramen-menu');
+ 
+ 
+ 
+ 
+// function addRamenImage(){
+//     const getDivId = document.getElementById('ramen-menu');
+//     let path = 'ramens.images';
+//     const images =
+// }
+ 
+ 
+ 
+// addEventListener('DOMContentLoaded', () => {
+   
+//     const URL = 'http://localhost:3000/ramens'
+   
+//     function fetchRamenList() {
+//         fetch(URL)
+//         .then(res => res.json)
+//         .then(json => renderRamenImageToScreen(json))
+//     }
+//     fetchRamenList();
+// })
+ 
+//     const renderRamenImageToScreen = ramens => {
+//         const ramenMenuContainer = document.getElementById('ramen-menu');
+//         ramens.forEach(ramen =>{
+//             const ramenMenu =
+//         })
+//     }
+ 
+// function createRamenImage(image) {
+//     image = document.querySelector(ramens.image);
+//     appendChild
+//     console.log(image)
+// }
+ 
+ 
+// const ramenDetailImage= document.querySelector('.detail-image');
+// console.log('hi')
+// fetch(ramenList)
+// .then((res) => console.log(HI))
+// .then(console.log(HI))
+ 
+// const ramenMenu = document.getElementById('ramen-menu')
+ 
+// function renderRamenImg(ramens) {
+ 
+// }
+ 
+// const renderRamenToScreen = ramens => {
+//     const ramenDiv = document.getElementById('ramen-menu');
+//     ramens.forEach(ramen => {
+//         const ramenMenu = makeRamenMenu(ramen);
+//         ramenDiv.append(ramenMenu)
+       
+//     })
+   
+// }
